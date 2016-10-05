dbd8fe3fe74587bb629977fdb5651ebe3309bae8
Thomas Anderer <thomas.anderer@andrena.de>
2016-10-05 17:55:59 +0200

    changes

--- src/main/java/org/pongmatcher/domain/MatchRequest.java
+++ src/main/java/org/pongmatcher/domain/MatchRequest.java
@@ -3,6 +3,8 @@ package org.pongmatcher.domain;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.GenerationType;
 
 @Entity
 @Table(name = "`match_request`")
